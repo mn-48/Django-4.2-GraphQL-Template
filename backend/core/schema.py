09ca7578ad16d9b8df7abd7312e16fa06482d262
mn-48 <nazmul.cse48@gmail.com>
2023-10-25 11:01:40 +0600

    core.schema update

--- backend/core/schema.py
+++ backend/core/schema.py
@@ -1,9 +1,15 @@
-
 import graphene
 
 from graphql_auth.schema import UserQuery, MeQuery
+from graphql_auth import mutations
+
+class AuthMutation(graphene.ObjectType):
+   register = mutations.Register.Field()
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(AuthMutation, graphene.ObjectType):
+   pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
